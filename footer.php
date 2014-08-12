edc29dfe5d379fb3e52f36f070e47f1194d635ff
hara reona <harareona@gmail.com>
2014-08-12 23:00:43 +0900

    footerファイル追加

---
+++ footer.php
@@ -0,0 +1,9 @@
+  <footer>
+    <hr>
+    <div class="site-info">
+      <p>Copyright &copy; <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a> All Rights Reserved. </p>
+    </div>
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
