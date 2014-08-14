d0c855147af791f78d57223a642aa77707c60378
hara reona <harareona@gmail.com>
2014-08-14 13:06:42 +0900

    404ページテンプレート追加

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+  <div id="main">
+    <div id="contents" class="thirteen offset-by-one">
+      <p>申し訳ございませんがお探しのページがみつかりません</p>
+      <p><a href="<?php echo home_url('/'); ?>">トップページ</a>へお戻りください</p>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
