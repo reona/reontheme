b038611d492b85ae97bad02a7fbcd03b76d8f87e
hara reona <harareona@gmail.com>
2014-08-19 13:19:17 +0900

    sns系が上手く共有されていなかった問題に対応

--- single.php
+++ single.php
@@ -8,8 +8,8 @@
           <h2><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
           <p class="date"><?php the_time('Y/n/j'); ?></p>
           <ul class="soc">
-            <li><a class="soc-twitter" href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php echo trim(wp_title( '', false)); ?>&via=your-twitter-id" target="_blank"></a></li>
-            <li><a class="soc-facebook" href="https://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&t=<?php echo trim(wp_title( '', false)); ?>" target="_blank"></a></li>
+            <li><a class="soc-twitter" href="https://twitter.com/share?url=<?php the_permalink(); ?>&text=<?php the_title(); ?>&via=your-twitter-id" target="_blank"></a></li>
+            <li><a class="soc-facebook" href="https://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>&t=<?php the_title(); ?>" target="_blank"></a></li>
             <li><a class="soc-google soc-icon-last" href="https://plus.google.com/share?url=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"></a></li>
           </ul>
         </div>
